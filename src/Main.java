86de47530f2a003175968d140337d2a0b041d1ad
iggypak <igypak@gmail.com>
2024-01-21 16:52:45 +0300

    add requested changes

--- src/Main.java
+++ src/Main.java
@@ -15,12 +15,17 @@ public class Main {
         }
 
         // powder - 400 g, sugar - 10 g, milk - 1 l, oil - 30 ml
-        System.out.println("Pancakes");
+        if (powderAmount >= 400 && sugarAmount >= 10 && milkAmount >= 1000 && oilAmount >= 30) {
+            System.out.println("Pancakes");
+        }
 
         // milk - 300 ml, powder - 5 g, eggs - 5
-        System.out.println("Omelette");
-
+        if (powderAmount >= 5 && milkAmount >= 300 && eggsCount >= 5) {
+            System.out.println("Omelette");
+        }
         // apples - 3, milk - 100 ml, powder - 300 g, eggs - 4
-        System.out.println("Apple pie");
+        if (powderAmount >= 300 && milkAmount >= 100 && appleCount >= 3 && eggsCount >= 4) {
+            System.out.println("Apple pie");
+        }
     }
 }
