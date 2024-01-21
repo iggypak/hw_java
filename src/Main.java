2a198c2cd6c9a93339626715ebf1153c42d3873c
iggypak <igypak@gmail.com>
2024-01-21 17:45:53 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,45 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int vasyaAge = 49;
+        int katyaAge = 34;
+        int mishaAge = 36;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int min = -1;
+        int middle = -1;
+        int max = -1;
+
+
+        if (vasyaAge >= katyaAge && vasyaAge >= mishaAge){
+            max = vasyaAge;
+            if(mishaAge <= katyaAge){
+                middle = katyaAge;
+                min = mishaAge;
+            } else {
+                middle = mishaAge;
+                min = katyaAge;
+            }
+        } else if (katyaAge >= mishaAge){
+            max = katyaAge;
+            if(vasyaAge <= mishaAge){
+                middle = mishaAge;
+                min = vasyaAge;
+            } else {
+                middle = vasyaAge;
+                min = mishaAge;
+            }
+        } else {
+            max = mishaAge;
+            if(vasyaAge <= katyaAge){
+                middle = katyaAge;
+                min = vasyaAge;
+            } else {
+                middle = vasyaAge;
+                min = katyaAge;
+            }
         }
+
+        System.out.println("Minimal age: " + min + "\nMiddle age: " + middle + "\nMiximal age: " + max);
+
     }
 }
