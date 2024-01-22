4ac37d560af5785fcba777521f271011ceffe7fb
iggypak <igypak@gmail.com>
2024-01-22 20:46:02 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.print("Введите число для нахождения пар множителей, произведением которых его можно получить: ");
+        int value = new Scanner(System.in).nextInt();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 1; i <= value; i = i + 1){
+            for (int j = 1; j <= value; j = j + 1){
+                if (i * j == value){
+                    System.out.println(i + " * " + j);
+                }
+            }
         }
     }
 }
