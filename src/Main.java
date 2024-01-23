b8e9dadfd77acba94414d2abb19d5d4229f44ad6
iggypak <igypak@gmail.com>
2024-01-23 10:05:45 +0300

    Task 4

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int value = new Random().nextInt(100);
+        int attempt = -1;
+        while (true) {
+            System.out.print("Угадайте число: ");
+            attempt = new Scanner(System.in).nextInt();
+            if (attempt < value){
+                System.out.println("Загаданное число больше ");
+            } else if (attempt > value) {
+                System.out.println("Загаданное число меньше ");
+            } else {
+                System.out.println("Вы угадали!");
+                break;
+            }
         }
     }
 }
